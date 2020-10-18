960bf8860acfd3f2a5f7f2479d3d3860fe7d8464
murakami-se <68378711+murakami-se@users.noreply.github.com>
2020-10-18 13:43:04 +0900

    Update cake_blog.sql

--- config/schema/cake_blog.sql
+++ config/schema/cake_blog.sql
@@ -1,27 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 5.0.2
--- https://www.phpmyadmin.net/
---
--- ホスト: 127.0.0.1
--- 生成日時: 2020-10-16 03:30:10
--- サーバのバージョン： 10.4.14-MariaDB
--- PHP のバージョン: 7.3.22
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- データベース: `sso_sample1`
---
-CREATE DATABASE IF NOT EXISTS `sso_sample1` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
-USE `sso_sample1`;
 
 -- --------------------------------------------------------
 
