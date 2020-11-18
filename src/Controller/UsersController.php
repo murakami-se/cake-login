0bbcc40e932b619f417f46f9fd7c32c03d4b7c5d
comee-system <system@comee.jp>
2020-11-18 15:22:21 +0900

    add passport sso login

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -124,7 +124,7 @@ class UsersController extends AppController
     {
         parent::beforeFilter($event);
         // auth off
-        $this->Auth->allow(['login', 'add', 'twgLoginCallback']);
+        $this->Auth->allow(['login', 'add', 'twgLoginCallback', 'passportLoginCallback']);
         // csrf off
         if($this->request->action == 'twgLoginCallback'){
             $this->getEventManager()->off($this->Csrf);
@@ -158,10 +158,10 @@ class UsersController extends AppController
 
     public function twgLoginCallback()
     {
-        if ($this->request->getData())
+        if ($this->request->query())
         {
             // アクセストークン取得
-            $token = $this->request->getData('access_token');
+            $token = $this->request->query('access_token');
             // curlコマンドでユーザー情報取得
             $headers = ["Authorization: Bearer " . $token ];
             $ch = curl_init("http://res.comee.ml/api/userinfo");
@@ -174,27 +174,99 @@ class UsersController extends AppController
             {
                 $result = json_decode($result, true);
                 // メールアドレスで登録状況確認
-                $user = $this->Users->find()->where(['email' => $result['email']])->first();
-                if ($user)
-                {
-                    // 登録済みの場合、ログイン
-                    $this->Auth->setUser($user);
-                } else {
-                    // 登録されていない場合、パスワードを補完してユーザー登録
-                    $pw = substr(str_shuffle('1234567890abcdefghijklmnopqrstuvwxyz'), 0, 8);
-                    $user = $this->Users->newEntity();
-                    $user->name = $result['name'];
-                    $user->email = $result['email'];
-                    $user->password = $pw;
-                    if ($this->Users->save($user)) {
-                        // 登録に成功した場合、ログイン
-                        $this->Auth->setUser($user);
-                    } else {
-                        $this->Flash->error(__('The user could not be saved. Please, try again.'));
-                    }
-                }
-                return $this->redirect($this->Auth->redirectUrl());
+                return $this->ssoUserLogin($result['email'], $result['name']);
             }
         }
+    }
+
+    public function passportLoginCallback()
+    {
+        if ($this->request->query())
+        {
+            // 認可コード取得
+            if ($code = $this->request->query('code')) {
+                // アクセストークン要求
+                $data = [
+                    'grant_type' => 'authorization_code',
+                    'client_id' => '1',
+                    'client_secret' => '92APPFrBgfb3p24yZiWiDtAf9neoz8M7iB4ZrGWd',
+                    'redirect_uri' => 'http://sample-site-a.comee.ml/users/passport-login-callback',
+                    'code' => $code
+                ];
+                $ch = curl_init('http://passport-auth-sample.comee.ml/oauth/token');
+                curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
+                curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+                $result = curl_exec($ch);
+                curl_close($ch);
+
+                // アクセストークン取得
+                if ($result) {
+                    $result = json_decode($result, true);
+                    if ($token = $result['access_token']) {
+                        // $expires = $result['expires'];   // アクセストークン有効期限チェック
+                        // 期限切れの場合の処理
+
+                        // ユーザー情報要求
+                        $headers = ["Authorization: Bearer " . $token ];
+                        $ch = curl_init("http://passport-auth-sample.comee.ml/api/user");
+                        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+                        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+                        $result = curl_exec($ch);
+                        curl_close($ch);
+                        
+                        // ユーザー情報取得
+                        if ($result)
+                        {
+                            $result = json_decode($result, true);
+                            if ($result['email'] && $result['name']) {
+                                return $this->ssoUserLogin($result['email'], $result['name']);
+                            } else {
+                                echo 'ユーザー情報の取得に失敗';
+                                exit;
+                            }
+                        } else {
+                            echo 'ユーザー情報取得curlに失敗';
+                            exit;
+                        }
+                    } else {
+                        echo 'アクセストークンの取得に失敗';
+                        exit;
+                    }
+                } else {
+                    echo 'アクセストークン取得curlに失敗';
+                    exit;
+                }
+            } else {
+                echo '認可コードの取得に失敗';
+                exit;
+            }
+        } else {
+            return $this->redirect(['action' => 'login']);
+        }
+    }
+
+    private function ssoUserLogin($email, $name) {
+        // メールアドレスで登録状況確認
+        $user = $this->Users->find()->where(['email' => $email])->first();
+        if ($user)
+        {
+            // 登録済みの場合、ログイン
+            $this->Auth->setUser($user);
+        } else {
+            // 登録されていない場合、パスワードを補完してユーザー登録
+            $pw = substr(str_shuffle('1234567890abcdefghijklmnopqrstuvwxyz'), 0, 8);
+            $user = $this->Users->newEntity();
+            $user->name = $name;
+            $user->email = $email;
+            $user->password = $pw;
+            if ($this->Users->save($user)) {
+                // 登録に成功した場合、ログイン
+                $this->Auth->setUser($user);
+            } else {
+                echo 'ユーザー登録に失敗';
+                exit;
+            }
+        }
+        return $this->redirect($this->Auth->redirectUrl());
     }
 }
--- src/Template/Users/login.ctp
+++ src/Template/Users/login.ctp
@@ -6,10 +6,13 @@
     <?= $this->Form->control('name') ?>
     <?= $this->Form->control('password') ?>
   </fieldset>
-  <?= $this->Form->button(__('Login')); ?>
+  <div style="margin: 20px;">
+    <?= $this->Form->button(__('Login')); ?>
+  </div>
   <?= $this->Form->end() ?>
 </div>
-<div class="">
-    <?= $this->Html->link('create', 'users/add') ?>
-    <?= $this->Html->link('sso-login', 'http://auth.comee.ml/authorization?response_type=token&client_id=19273204454143&response_mode=form_post&redirect_uri=https://sample-site-a.comee.ml/users/twg-login-callback&scope=openid+email+profile&nonce=lkdjafjlkdsja') ?>
+<div style="margin: 22px; text-align: right">
+    <a href="http://passport-auth-sample.comee.ml/oauth/authorize?client_id=1&response_type=code&scope=*&redirect_uri=http://sample-site-a.comee.ml/users/passport-login-callback" class="btn" style="background-color: navy;">Passport Sample Login</a>
+    <a href="http://auth.comee.ml/authorization?response_type=token&client_id=19273204454143&response_mode=form_post&redirect_uri=https://sample-site-a.comee.ml/users/twg-login-callback&scope=openid+email+profile&nonce=lkdjafjlkdsja" class="btn" style="background-color: green;">Authlete Sample Login</a>
+    <a href="/users/add" class="btn" style="background-color: maroon;">Rigister</a>
 </div>
